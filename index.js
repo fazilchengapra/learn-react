4fabb684268b1c24678e4a885fbd5a7ccf3dca13
Fazil Chengapra <muhammedfazilchengapra@gmail.com>
2024-09-02 18:07:39 +0530

    episode-2

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 const h1 = React.createElement('div', { id: 'parant' }, [
     React.createElement('div', { id: 'chaild1' },
         [React.createElement('h1', {}, 'I am a h1 tag'), React.createElement('h2', {}, 'I am a h2 tag')]),
     React.createElement('div', { id: 'chaild2' },
         [React.createElement('h1', {}, 'I am a h1 tag'), React.createElement('h2', {}, 'I am a h2 tag')])
-])
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(h1)
+root.render(h1);
